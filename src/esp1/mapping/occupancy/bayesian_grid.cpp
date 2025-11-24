cf59a246978f66c3def4ef4230bb7fd399649484
alexdorsaz <160587479+alexdorsaz@users.noreply.github.com>
2025-11-25 00:32:34 +0100

    Squashed commit of the following:
    commit 7c33af43ce425e269b1d63dc6739d2d876ab9c59
    Author: Anas El Madhi <anas.elmadhi@epfl.ch>
    Date:   Sun Nov 16 23:16:29 2025 +0100
    
        Sim and mapping updates:
        - Added full SFML-based visual test for BayesianOccupancyGrid (static + dynamic)
        - Updated bayesian_grid.cpp/h with safer bounds and cleaner log-odds update
        - Refactored data_types.h to support dual compilation (ESP32 Arduino + PC)
        - Removed direct Arduino.h dependency using ARDUINO guard
        - Added LiDAR simulation and visualization improvements

---
+++ src/esp1/mapping/occupancy/bayesian_grid.cpp
@@ -0,0 +1,137 @@
+#include "../../../../include/esp1/mapping/occupancy/bayesian_grid.h"
+#include <cmath>
+#include <algorithm>
+
+#define LOG_ODDS_FREE     -0.4f
+#define LOG_ODDS_OCCUPIED  0.4f
+#define LOG_ODDS_MIN       -4.0f
+#define LOG_ODDS_MAX        4.0f
+
+// ------------------------------------------------------
+// Constructor
+// ------------------------------------------------------
+BayesianOccupancyGrid::BayesianOccupancyGrid(float resolution_m,
+                                             uint16_t size_x,
+                                             uint16_t size_y)
+{
+    // Sécurité : ne jamais dépasser la taille max
+    if (size_x > GRID_MAX_X) size_x = GRID_MAX_X;
+    if (size_y > GRID_MAX_Y) size_y = GRID_MAX_Y;
+
+    grid_resolution = resolution_m;
+    grid_size_x = size_x;
+    grid_size_y = size_y;
+    log_odds = new float[grid_size_x * grid_size_y];
+
+    // Initialise toute la grille à 0
+    for (int i = 0; i < grid_size_x * grid_size_y; i++)
+        log_odds[i] = 0.0f;
+}
+
+// ------------------------------------------------------
+// Update log occupancy grid
+// ------------------------------------------------------
+void BayesianOccupancyGrid::update_map(const LiDARScan& scan,
+                                       const Pose2D& pose, const float lidar_max_range)
+{
+    for (const auto& landmark : scan.landmarks)
+    {
+        float angle_world = pose.theta + landmark.angle;
+
+        float r = landmark.range; 
+        bool is_hit = (r > 0);
+        if(!is_hit){
+            r = lidar_max_range;
+        }
+
+        float hit_x = pose.x + r * std::cos(angle_world);
+        float hit_y = pose.y + r * std::sin(angle_world);
+
+        int x0 = (int)(pose.x / grid_resolution) + grid_size_x / 2;
+        int x1 = (int)(hit_x / grid_resolution) + grid_size_x / 2;
+        int y0 = (int)(-pose.y / grid_resolution) + grid_size_y / 2;
+        int y1 = (int)(-hit_y  / grid_resolution) + grid_size_y / 2;
+
+
+        // Bresenham algorithm
+        int dx = std::abs(x1 - x0);
+        int dy = std::abs(y1 - y0);
+        int sx = (x0 < x1) ? 1 : -1;
+        int sy = (y0 < y1) ? 1 : -1;
+        int err = dx - dy;
+
+        int x = x0;
+        int y = y0;
+
+        while (true)
+        {
+            if (x == x1 && y == y1)
+                break;
+
+            if (x >= 0 && x < grid_size_x && y >= 0 && y < grid_size_y)
+            {
+                int idx = y * grid_size_x + x;
+                log_odds[idx] = std::max(LOG_ODDS_MIN,
+                                         log_odds[idx] + LOG_ODDS_FREE);
+            }
+
+            int e2 = 2 * err;
+            if (e2 > -dy) { err -= dy; x += sx; }
+            if (e2 < dx)  { err += dx; y += sy; }
+        }
+
+        // Cellule finale occupée
+        if(is_hit){
+            if (x1 >= 0 && x1 < grid_size_x && y1 >= 0 && y1 < grid_size_y){
+                int idx = y1 * grid_size_x + x1;
+                log_odds[idx] = std::min(LOG_ODDS_MAX, log_odds[idx] + LOG_ODDS_OCCUPIED);
+            }
+        }
+    }
+}
+
+// ------------------------------------------------------
+// Probability (not log)
+// ------------------------------------------------------
+float BayesianOccupancyGrid::get_cell_probability(float x_idx,
+                                                   float y_idx) const
+{
+    int x = (int)x_idx;
+    int y = (int)y_idx;
+
+    if (x < 0 || x >= grid_size_x || y < 0 || y >= grid_size_y)
+        return 0.5f;
+
+    float L = log_odds[y * grid_size_x + x];
+    float odds = std::exp(L);
+    return odds / (1.0f + odds);
+}
+
+// ------------------------------------------------------
+// Export compressed map (0–255 grayscale)
+// ------------------------------------------------------
+const uint8_t* BayesianOccupancyGrid::get_map_data_color() const
+{
+    static uint8_t export_data[GRID_MAX_SIZE];
+
+    for (int y = 0; y < grid_size_y; y++)
+    {
+        for (int x = 0; x < grid_size_x; x++)
+        {
+            float prob = get_cell_probability(x, y);
+            float v = 255.0f * (1 - prob);
+
+            if (v < 0) v = 0;
+            if (v > 255) v = 255;
+
+            export_data[y * grid_size_x + x] = (uint8_t)v;
+        }
+    }
+
+    return export_data;
+}
+
+const float* BayesianOccupancyGrid::get_map_data() const
+{
+    return log_odds;
+}
