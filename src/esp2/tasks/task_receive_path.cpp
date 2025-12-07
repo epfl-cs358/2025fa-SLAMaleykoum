231511b1f3989af05c29240956bbaf8d742eb5f6
cmaisonn <160581266+clea-m@users.noreply.github.com>
2025-12-07 22:01:00 +0100

    Main Task ESP2 : removed mqtt from receive path task

--- src/esp2/tasks/task_receive_path.cpp
+++ src/esp2/tasks/task_receive_path.cpp
@@ -3,16 +3,8 @@
 #include "common/esp_link.h"
 #include "common/data_types.h"
 
-#include "common/wifi_connection.h"
-
-char msgReceive[100];
-const char* mqtt_topic_connection_esp2_receive= "slamaleykoum77/setupesp2";
-
 void TaskReceivePath(void *pvParameters) {
 
-    connection.setupWifi();
-    connection.check_connection();
-    connection.publish(mqtt_topic_connection_esp2_receive, "Wifi Setup success");
     for (;;) {
         esp_link.poll();
         GlobalPathMessage gpm;
@@ -23,14 +15,6 @@ void TaskReceivePath(void *pvParameters) {
             if (!firstPathReceived) {
                 firstPathReceived = true;
             }
-            
-            for (uint16_t i = 0; i < gpm.current_length; i++) {
-                    connection.check_connection();
-                    snprintf(msgReceive, sizeof(msgReceive), 
-                             "Waypoint %d: x = %.2f, y = %.2f", 
-                             i, gpm.path[i].x, gpm.path[i].y);
-
-                    connection.publish(mqtt_topic_connection_esp2_receive, msgReceive);}
         }
         vTaskDelay(pdMS_TO_TICKS(10));
     }
